c0b2dae4826da814de3f1d6df52c906bf74f9956
Gabriel David <93693638+chaosisfair@users.noreply.github.com>
2021-12-04 14:34:55 -0800

    Add last name to user_name in py file

--- userinterfacepro.py
+++ userinterfacepro.py
@@ -1,3 +1,3 @@
-user_name = input("What is your name? ")
+user_name = input("What is your first and last name? ")
 user_age = input("What is your age? ")
 user_weight = input("What is your weight? ")
